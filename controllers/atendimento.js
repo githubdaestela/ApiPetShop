3c959ce74e9403ed1782a6825488963c58c6d312
gitdaestela <estelacriart@gmail.com>
2021-05-18 13:19:57 -0300

    Created: nodemon e rota atendimento

---
+++ controllers/atendimento.js
@@ -0,0 +1,4 @@
+module.exports = app => {
+    app.get('/atendimentos', (req,res) => res.send('Você está na rota de atendimentos está realizando um GET'))
+    app.post('/atendimentos', (req,res) => res.send('Você está na rota de atendimentos e está realizando um POST'))   
+}
